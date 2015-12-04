44351edd09ee11b7986354bc1bd8058a39e480d9
Kevin <kevlee.1994@gmail.com>
2015-12-04 11:27:33 -0500

    eee

--- app/src/main/java/course/example/ben/hobosigns/Home.java
+++ app/src/main/java/course/example/ben/hobosigns/Home.java
@@ -1,8 +1,12 @@
 package course.example.ben.hobosigns;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -15,9 +19,12 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.GoogleMap.CancelableCallback;
 import com.google.android.gms.maps.GoogleMap.OnCameraChangeListener;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.Circle;
@@ -27,16 +34,21 @@ import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import com.parse.ParseException;
+import com.parse.ParseGeoPoint;
 import com.parse.ParseUser;
+
+import java.util.HashSet;
 
 /**
  * Created by Ben on 11/17/2015.
  * Home is the main interaction with a user.
  * The user will view post of other user in the area via map or list mode
  */
-public class Home extends AppCompatActivity {
+public class Home  extends FragmentActivity implements OnMapReadyCallback {
     private SupportMapFragment mapFragment;
     private LocationRequest locationRequest;
+    private String TAG = "Testing";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +61,7 @@ public class Home extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(Home.this, CreatePost.class);
                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                 startActivity(intent);
             }
         });
@@ -57,8 +70,8 @@ public class Home extends AppCompatActivity {
 
         // Set up the map fragment
         mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map_fragment);
-        // Enable the current location "blue dot"
-        //mapFragment.getMap().setMyLocationEnabled(true);
+        mapFragment.getMapAsync(this);
+        Log.i(TAG, mapFragment.toString());
 
     }
 
@@ -87,6 +100,20 @@ public class Home extends AppCompatActivity {
         Intent intent = new Intent(Home.this, WelcomeScreen.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        LatLng collegePark = new LatLng(38.99, -76.92);
+        LatLng test = new LatLng(38.99, -76.82);
+        BitmapDescriptor icon = BitmapDescriptorFactory.fromResource(R.drawable.test);
+        BitmapDescriptor icon1 = BitmapDescriptorFactory.fromResource(R.drawable.test1);
+        //TODO: DO THIS WITH BITMAPS FROM DATABASE - PUT IN QUERY METHOD
+        //BitmapDescriptorFactory.fromBitmap();
+        map.addMarker(new MarkerOptions().position(collegePark).title("Test Marker").icon(icon));
+        map.addMarker(new MarkerOptions().position(test).title("Test 1").icon(icon1));
+        map.moveCamera(CameraUpdateFactory.newLatLng(collegePark));
+        map.animateCamera(CameraUpdateFactory.zoomTo(12.0f));
     }
 
 
---
+++ app/src/main/java/course/example/ben/hobosigns/MapsActivity.java
@@ -0,0 +1,30 @@
+package course.example.ben.hobosigns;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        // Add a marker in Sydney, Australia, and move the camera.
+        LatLng collegePark = new LatLng(38.99, -76.92);
+        map.addMarker(new MarkerOptions().position(collegePark).title("Test Marker"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(collegePark));
+        map.animateCamera(CameraUpdateFactory.zoomTo(12.0f));
+    }
+}
