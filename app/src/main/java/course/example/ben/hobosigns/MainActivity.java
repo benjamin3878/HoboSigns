faa3add08d1bb6052e23500e5848fd664bb86790
Enoch Hsiao <enoch.hsiao@gmail.com>
2015-12-06 19:53:00 -0500

    resetting weird git stash issue, sorry

--- app/src/main/java/course/example/ben/hobosigns/MainActivity.java
+++ app/src/main/java/course/example/ben/hobosigns/MainActivity.java
@@ -1,6 +1,9 @@
 package course.example.ben.hobosigns;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Point;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,6 +11,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import com.parse.Parse;
 import com.parse.ParseObject;
